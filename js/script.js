b3adf6b3b14c8449958752d409fc06a19bc4cf44
lindam <linda.minotti2@gmail.com>
2020-05-25 17:16:34 +0200

    Creato il gioco dei dadi con verifica dell'input.

--- js/script.js
+++ js/script.js
@@ -44,6 +44,7 @@
 var buttonInviaEmail = document.getElementById('invio_email');
 listaEmail = ['linda@gmail.com', 'alice@gmail.com', 'pippo@gmail.com', 'topolino@gmil.com', 'minnie@gmail.com', 'paperina@gmail.com'];
 var controlloEmail = false;
+var buttonLanciaDado = document.getElementById('lancia_dado');
 
 // Creo evento per entrare nel gioco se la mail è presente nella lista
 buttonInviaEmail.addEventListener('click',
@@ -59,6 +60,31 @@ buttonInviaEmail.addEventListener('click',
     // Stampa risultato del controllo emailUtente
     if (controlloEmail) {
       console.log('Puoi entrare!');
+      // SCRIPT PER IL GIOCO DEI DADI
+      // Crea variabili numeri dei dadi
+      // Creo evento per lanciare il lancia_dado
+      buttonLanciaDado.addEventListener('click',
+        function () {
+          var numeroGiocatore = document.getElementById('numero_utente').value;
+          console.log('Numero giocatore: ' + numeroGiocatore);
+          var numeroPc = Math.floor(Math.random() * 6) + 1;
+          console.log('Numero generato dal pc: ' + numeroPc);
+
+          // Controllo del vincitore ai dadi
+          if (numeroGiocatore > 0 && numeroGiocatore < 7) {
+            if (numeroGiocatore > numeroPc) {
+              console.log('Ha vinto il giocatore!');
+            } else if (numeroGiocatore < numeroPc) {
+              console.log('Ha vinto il PC!');
+            } else {
+              console.log ('Parimerito!!');
+            }
+          } else {
+            alert('Errore: il numero inserito non è valido per il gioco dei dadi.');
+            }
+        }
+    )
+
     } else {
       console.log('La tua mail non è in lista!');
     }
