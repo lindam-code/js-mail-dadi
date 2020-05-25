d4d6a1abf08d391020aed56991cd5cdac4ff6e19
lindam <linda.minotti2@gmail.com>
2020-05-25 18:16:04 +0200

    Gioco quasi ultimato.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // INIZIO SCRIPT PER IL CONTROLLO DELL'EMAIL
 
-// Inizializzazione variabili
+// Dichiarazione variabili
 // var emailUtente = prompt('Scrivi la tua email');
 // listaEmail = ['linda@gmail.com', 'alice@gmail.com', 'pippo@gmail.com', 'topolino@gmil.com', 'minnie@gmail.com', 'paperina@gmail.com'];
 // var controlloEmail = false;
@@ -40,17 +40,20 @@
 // // Fine script per il controllo dell'email
 
 // PROVA SENZA prompt
-// Inizilizzazione variabili
+// Dichiarazione variabili generali
 var buttonInviaEmail = document.getElementById('invio_email');
 listaEmail = ['linda@gmail.com', 'alice@gmail.com', 'pippo@gmail.com', 'topolino@gmil.com', 'minnie@gmail.com', 'paperina@gmail.com'];
 var controlloEmail = false;
 var buttonLanciaDado = document.getElementById('lancia_dado');
 var giocoContainer = document.getElementById('gioco_container');
+var messaggioEmailErrata = document.getElementById('errore_email');
+var risultatoGioco;
+
 
 // Creo evento per entrare nel gioco se la mail è presente nella lista
 buttonInviaEmail.addEventListener('click',
   function () {
-    // Inizializzazione variabile email
+    // Dichiarazione variabile email
     var emailUtente = document.getElementById('email_utente').value;
     // Ciclo for per iterare l'array listaEmail e controllare se l'email data dall'utente è presente
     for (var i = 0; i < listaEmail.length; i++) {
@@ -58,37 +61,62 @@ buttonInviaEmail.addEventListener('click',
         controlloEmail = true;
       }
     }
-    // Stampa risultato del controllo emailUtente
+    // Risultato del controllo email utente
     if (controlloEmail) {
       console.log('Puoi entrare!');
       giocoContainer.className = 'visible';
+      messaggioEmailErrata.className = 'hidden';
+
       // SCRIPT PER IL GIOCO DEI DADI
-      // Crea variabili numeri dei dadi
+
       // Creo evento per lanciare il lancia_dado
       buttonLanciaDado.addEventListener('click',
         function () {
+
+          // Dichiarazione variabili per il gioco
           var numeroGiocatore = document.getElementById('numero_utente').value;
-          console.log('Numero giocatore: ' + numeroGiocatore);
+          console.log('Tuo numero' + numeroGiocatore);
+          var tuoNumero = document.getElementById('tuo_numero');
+
           var numeroPc = Math.floor(Math.random() * 6) + 1;
-          console.log('Numero generato dal pc: ' + numeroPc);
+          console.log('Numero PC' + numeroPc);
+          var numeroPcDocument = document.getElementById('numero_pc');
+
+
+          var risutatoDadi = document.getElementById('risultato_dadi');
+
 
           // Controllo del vincitore ai dadi
           if (numeroGiocatore > 0 && numeroGiocatore < 7) {
             if (numeroGiocatore > numeroPc) {
-              console.log('Ha vinto il giocatore!');
+              risutatoGioco = 'Hai vinto!!!';
+              console.log('hai vinto');
+              console.log(risultatoGioco);
             } else if (numeroGiocatore < numeroPc) {
+              risutatoGioco = 'Ha vinto il PC!';
               console.log('Ha vinto il PC!');
+              console.log(risultatoGioco);
             } else {
-              console.log ('Parimerito!!');
+              risutatoGioco ='Parimerito!!';
+              console.log('Parimerito!');
+              console.log(risultatoGioco);
             }
+
+            // Risultato del gioco
+            tuoNumero.innerHTML = 'Tuo numero è: ' + numeroGiocatore;
+            numeroPcDocument.innerHTML = 'Il numero generato dal PC è:  ' + numeroPc;
+            risutatoDadi.innerHTML = 'prova';
+
+          // messaggio errore se l'utente mette un numero non valido per il gioco
           } else {
             alert('Errore: il numero inserito non è valido per il gioco dei dadi.');
             }
         }
     )
-
+    // Messaggio di errore se l'email non è valida
     } else {
       console.log('La tua mail non è in lista!');
+      messaggioEmailErrata.className = 'visible';
     }
   }
 )
